1aed3c9e2df3f70750111ade1ef8fe136c49661e
Jandier Rojas Zúñiga <jandier.rojas.z@gmail.com>
2026-09-10 10:23:32 -0600

    Add the ResultTable component

---
+++ src/ResultTable.jsx
@@ -0,0 +1,44 @@
+import { calculateInvestmentResults, formatter } from './util/investment';
+
+export default function ResultTable({ userInput }) {
+  const resultsData = calculateInvestmentResults(userInput);
+  const initialInvestment =
+    resultsData[0].valueEndOfYear -
+    resultsData[0].interest -
+    resultsData[0].annualInvestment;
+  return (
+    <table id="result">
+      <thead>
+        <tr>
+          <th>Year</th>
+          <th>Investment value</th>
+          <th>Interest(Year)</th>
+          <th>Total Interest</th>
+          <th>Invested Capital</th>
+        </tr>
+      </thead>
+      <tbody>
+        {resultsData.map((yearData) => {
+          const totalInterest =
+            yearData.valueEndOfYear -
+            yearData.annualInvestment * yearData.year -
+            initialInvestment;
+          const totalAmountInvested = yearData.valueEndOfYear - totalInterest;
+
+          // I left off lecture 104, I finished, however, I have bug for the format of my output in the table.
+          //It might be something with the strings being concatenated with the numbers.
+
+          return (
+            <tr key={yearData.year}>
+              <td>{yearData.year}</td>
+              <td>{formatter.format(yearData.valueEndOfYear)}</td>
+              <td>{formatter.format(yearData.interest)}</td>
+              <td>{formatter.format(totalInterest)}</td>
+              <td>{formatter.format(totalAmountInvested)}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+}
--- src/UserInput.jsx
+++ src/UserInput.jsx
@@ -1,25 +1,49 @@
-export default function UserInput() {
-  const userInputArray = [[], [], [], []];
+export default function UserInput({ onChange, userInput }) {
   return (
     <section id="user-input">
       <div className="input-group">
         <p>
           <label>Initial investment</label>
-          <input type="number" />
+          <input
+            type="number"
+            required
+            value={userInput.initialInvestment}
+            onChange={(event) =>
+              onChange('initialInvestment', event.target.value)
+            }
+          />
         </p>
         <p>
           <label>Annual investment</label>
-          <input type="number" />
+          <input
+            type="number"
+            required
+            value={userInput.annualInvestment}
+            onChange={(event) =>
+              onChange('annualInvestment', event.target.value)
+            }
+          />
         </p>
       </div>
       <div className="input-group">
         <p>
           <label>Expected return</label>
-          <input type="number" step="0.01" />
+          <input
+            type="number"
+            step="0.01"
+            required
+            value={userInput.expectedReturn}
+            onChange={(event) => onChange('expectedReturn', event.target.value)}
+          />
         </p>
         <p>
           <label>Duration</label>
-          <input type="number" />
+          <input
+            type="number"
+            required
+            value={userInput.duration}
+            onChange={(event) => onChange('duration', event.target.value)}
+          />
         </p>
       </div>
     </section>
